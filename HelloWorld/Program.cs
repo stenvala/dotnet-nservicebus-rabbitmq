a5dd587fbfd0b5758971f4779d9638972bac0b14
Antti Stenvall <antti@stenvall.fi>
2021-04-25 21:39:05 +0300

    Rename folder

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            AsyncMain().GetAwaiter().GetResult();
+        }
+
+        static async Task AsyncMain()
+        {
+            Console.WriteLine("Initializing queue!");                       
+
+            var endpointInstance = await BasicQueuing.ConfigureBasicEndpoint("MyFirstQueue");            
+
+            Console.WriteLine("Starting looping!");
+            await BasicQueuing.RunLoop(endpointInstance).ConfigureAwait(false);
+            // When done
+            await endpointInstance.Stop().ConfigureAwait(false);            
+        }               
+    }    
+}
